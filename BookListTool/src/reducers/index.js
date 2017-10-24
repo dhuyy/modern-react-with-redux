2daeffa94e85d6acb00dc8df6e6a570a0e7e9d66
Vandhuy <vandhuy@gmail.com>
2017-10-24 13:26:27 -0300

    Added BookListTool website

---
+++ BookListTool/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import BooksReducer from './ReducerBooks';
+import ActiveBookReducer from './ReducerActiveBook';
+
+const rootReducer = combineReducers({
+  books: BooksReducer,
+  activeBook: ActiveBookReducer
+});
+
+export default rootReducer;
